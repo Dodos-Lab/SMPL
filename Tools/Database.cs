2de89c157c22c3ed7fa8b1cef46a599093e351e4
Georgi Kanchev <52318254+Dodos-Lab@users.noreply.github.com>
2022-05-05 16:12:26 +0300

    -

--- Tools/Database.cs
+++ Tools/Database.cs
@@ -1,54 +1,110 @@
 ﻿using Newtonsoft.Json;
 using SMPL.Tools;
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Numerics;
+using Console = SMPL.Tools.Console;
 
 namespace Test
 {
 	[JsonObject(MemberSerialization.Fields)]
 	public class Database
 	{
+		public class Sheet { }
+
 		[JsonObject(MemberSerialization.Fields)]
-		internal class Sheet<T>
+		internal class Sheet<T> : Sheet
 		{
 #pragma warning disable CS0649
 			internal string name;
 			internal T[] lines;
-#pragma warning disable CS0649
+			internal dynamic[] columns;
+			internal dynamic[] separators;
+			internal dynamic props;
 
-			internal Sheet(T[] lines) => this.lines = lines;
+			[JsonIgnore]
+			internal List<T> linesList;
+#pragma warning disable CS0649
 		}
 #pragma warning disable CS0649
 		internal dynamic[] sheets;
+		internal dynamic[] customTypes;
+		internal bool compress;
 #pragma warning disable CS0649
+		[JsonIgnore]
+		private Dictionary<string, Sheet> cacheSheets;
+		[JsonIgnore]
+		private string path;
 
 		public static Database Load(string cdbFilePath)
 		{
+			var db = default(Database);
 			try
 			{
-				var menu = File.ReadAllText(cdbFilePath);
-				return JsonConvert.DeserializeObject<Database>(menu.ToString());
+				db = JsonConvert.DeserializeObject<Database>(File.ReadAllText(cdbFilePath));
+				db.path = cdbFilePath;
 			}
 			catch { Console.LogError(1, $"Could not load {nameof(Database)} file with path '{cdbFilePath}'."); }
-			return default;
+			return db;
 		}
-		private Database() { }
 
-		public T GetLine<T>(string sheetName, int lineIndex)
+		public List<T> GetSheet<T>(string sheetName)
 		{
 			try
 			{
+				if (cacheSheets == null)
+					cacheSheets = new();
+
+				if (cacheSheets.ContainsKey(sheetName))
+					return ((Sheet<T>)cacheSheets[sheetName]).linesList;
+
 				for (int i = 0; i < sheets.Length; i++)
 					if (sheets[i].name == sheetName)
 					{
-						var sheet = JsonConvert.DeserializeObject<Sheet<T>>(sheets[i].ToString());
-						return sheet.lines[lineIndex];
+						var json = sheets[i].ToString();
+						var sheet = (Sheet<T>)JsonConvert.DeserializeObject<Sheet<T>>(json);
+						var linesList = sheet.lines.ToList();
+						cacheSheets[sheetName] = sheet;
+						((Sheet<T>)cacheSheets[sheetName]).linesList = linesList;
+						return linesList;
 					}
 			}
 			catch (System.Exception) { }
-			Console.LogError(1, $"Could not find line with {nameof(lineIndex)} '{lineIndex}' in " +
-				$"{nameof(Sheet<T>)}<{typeof(T)}> with {nameof(sheetName)} '{sheetName}' in this {nameof(Database)}.");
+			Console.LogError(1, $"Could not load {nameof(Sheet<T>)}<{typeof(T)}> with {nameof(sheetName)} '{sheetName}' in this {nameof(Database)}.");
 			return default;
+		}
+		public void AddSheet<T>(string sheetName) => cacheSheets[sheetName] = new Sheet<T>();
+		public void SaveSheet<T>(string sheetName)
+		{
+			if (cacheSheets.ContainsKey(sheetName) == false)
+			{
+				Console.LogError(1, $"No {nameof(Sheet)} with {nameof(sheetName)} '{sheetName}' was found in this {nameof(Database)}. " +
+					$"This may be because a {nameof(Sheet)}\nwith such name does not exist or it is not yet loaded into this {nameof(Database)} " +
+					$"(by {nameof(GetSheet)}({nameof(sheetName)})).");
+				return;
+			}
+
+			var isNew = true;
+			for (int i = 0; i < sheets.Length; i++)
+				if (sheets[i].name == sheetName)
+				{
+					sheets[i] = cacheSheets[sheetName];
+					isNew = false;
+					break;
+				}
+
+			if (isNew)
+			{
+				var list = sheets.ToList();
+				list.Add(cacheSheets[sheetName]);
+				sheets = list.ToArray();
+			}
+			var sheet = ((Sheet<T>)cacheSheets[sheetName]);
+			sheet.lines = sheet.linesList.ToArray();
+			var json = JsonConvert.SerializeObject(this, Formatting.Indented);
+			File.WriteAllText(path, json);
 		}
 	}
 }
